9a79b57a720c3194d0d1709dfb704e58cf26345e
asif0777 <64778965+asif0777@users.noreply.github.com>
2021-06-25 15:37:33 +0530

    handled the cancelled response .

--- catalog/controller/extension/payment/layerpayment.php
+++ catalog/controller/extension/payment/layerpayment.php
@@ -126,8 +126,11 @@ class ControllerExtensionPaymentLayerpayment extends Controller {
 							function (response) {
 								console.log(response)
 								if(response !== null || response.length > 0 ){
-									if(response.payment_id !== undefined || response.payment_id !== '' || response.payment_id != null){
+									if(response.payment_id !== undefined && response.status !== 'cancelled'){
 										document.getElementById('layer_payment_id').value = response.payment_id;
+										document.layer_payment_int_form.submit();
+									}else if(response.payment_id !== undefined && response.status == 'cancelled'){
+										Layer.cancel;
 									}
 								}
 								document.layer_payment_int_form.submit();
